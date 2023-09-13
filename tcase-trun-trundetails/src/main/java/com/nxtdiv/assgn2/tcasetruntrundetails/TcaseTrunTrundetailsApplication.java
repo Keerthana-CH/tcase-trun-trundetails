18eb16de7d8fa28886d73c0ab91b10032520da65
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-13 03:17:40 -0700

    created a spring boot project

---
+++ tcase-trun-trundetails/src/main/java/com/nxtdiv/assgn2/tcasetruntrundetails/TcaseTrunTrundetailsApplication.java
@@ -0,0 +1,13 @@
+package com.nxtdiv.assgn2.tcasetruntrundetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcaseTrunTrundetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcaseTrunTrundetailsApplication.class, args);
+	}
+
+}
---
+++ tcase-trun-trundetails/src/test/java/com/nxtdiv/assgn2/tcasetruntrundetails/TcaseTrunTrundetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxtdiv.assgn2.tcasetruntrundetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcaseTrunTrundetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
